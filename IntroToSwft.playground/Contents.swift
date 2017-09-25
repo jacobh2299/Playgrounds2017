28e63e0589160277c80d63ff16ac837551ee8a56
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2017-09-25 08:31:59 -0600

    made thing mr. teacherman told me to make and then made a var name bonelessBizza that prints a good meme

---
+++ IntroToSwft.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//Explicit Type Declaration with no value
+var ramen :String
+//Implicit Type declration with a value to assign type.
+
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge + 54322456
+var someValue = 3.1415 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+var bonelessBizza = "tryin to hop on that ranch?"
+print(bonelessBizza)
