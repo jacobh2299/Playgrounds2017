c02b0d2d870e1fc2b0c820f72cc12c6a102a0c18
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2017-09-25 08:55:07 -0600

    commented all my previous code and figured out how to use if statements

--- IntroToSwft.playground/Contents.swift
+++ IntroToSwft.playground/Contents.swift
@@ -5,28 +5,45 @@ import UIKit
 var ramen :String
 //Implicit Type declration with a value to assign type.
 
-var soup = "Ramen"
-ramen = soup
-print(ramen)
-print(soup)
-//ramen = 7
-var myAge :Int
-myAge = -89
-print(myAge)
-myAge = myAge + 54322456
-var someValue = 3.1415 * 56
-someValue = someValue * 231.1
-var isReal :Bool
-isReal = false
-isReal = true
-var bonelessBizza = "tryin to hop on that ranch?"
-print(bonelessBizza)
-var meme :String
-meme = "Does the bizza got bones?"
-var AnswerTrue = "It not a bonelessBizza"
-var AnswerFalse = "Dat a bonelessBizza my dude, go get the ranch."
-print(meme)
+//var soup = "Ramen"
+//ramen = soup
+//print(ramen)
+//print(soup)
+////ramen = 7
+//var myAge :Int
+//myAge = -89
+//print(myAge)
+//myAge = myAge + 54322456
+//var someValue = 3.1415 * 56
+//someValue = someValue * 231.1
+//var isReal :Bool
+//isReal = false
+//isReal = true
+//var bonelessBizza = "tryin to hop on that ranch?"
+//print(bonelessBizza)
+//var meme :String
+//meme = "Does the bizza got bones?"
+//var AnswerTrue = "It not a bonelessBizza"
+//var AnswerFalse = "Dat a bonelessBizza my dude, go get the ranch."
+//print(meme)
+//var bizza :Bool
+//bizza = false
+//print(bizza)
+//print(AnswerFalse)
+
 var bizza :Bool
-bizza = false
-print(bizza)
-print(AnswerFalse)
+var q :String
+q = "Is that a boneless Bizza?"
+print(q)
+bizza = true
+if bizza == true
+{
+    
+    print("wow, that a good bizza my dude, fetch the ranch.")
+    
+}
+else
+{
+    print("oh snap man take that bizza back to the shop cause it not good my dude.")
+}
+
